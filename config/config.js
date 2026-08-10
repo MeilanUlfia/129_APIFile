b98657129f651fec1445fe9946643ec32a99e5fc
meilan <meilanulfia@gmail.com>
2026-08-10 15:03:15 +0700

    perbaiki password

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const development = {
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
